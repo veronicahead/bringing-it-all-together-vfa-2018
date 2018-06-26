a09206111fd7aa42d51aec618f6b4e237bd59942
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 04:06:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,11 +49,11 @@ class Dog
     new_dog
   end
   
-  def self.new_from_db
+  def self.new_from_db(row)
     id = row[0]
     name = row[1]
     breed = row[2]
-    self.new (id: id, name: name, breed: breed)
+    self.new(id: id, name: name, breed: breed)
   end
   
   def self.find_by_id

01decb88dbd5ebf3ec25bc652c2cc43493ebc701
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 04:07:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,17 @@ class Dog
   end
   
   def self.find_by_name
-    
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
   
   def self.find_by_id
   end

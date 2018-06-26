c53e8dc13fa037eb5a91284fe071d616e6f45366
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 04:09:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,7 +69,17 @@ class Dog
     end.first
   end
   
-  def self.find_by_id
+  def self.find_by_id (id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql,id).map do |row| self.new_from_db(row)
+    end.first
+  end
   end
   
 end

fb05e12e4d643e40b3bcb8dfdd68d3189ad7ea65
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:58:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,6 +29,11 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id, name, breed)
+  end
   
 end

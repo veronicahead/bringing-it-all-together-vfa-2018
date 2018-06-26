cd0c8d2c4f089aee563ca1aecf63edb43fd4700f
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-26 03:47:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
 class Dog
   
+  attr_accessor id:, name:, breed:
+  def initialize (id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
   
+  def self.create_table
   
 end
